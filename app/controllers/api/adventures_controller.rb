1951788ad4b93aadcab7753d0677b1d3cdd98fd7
Steven Harlow <STHarlow@gmail.com>
2013-09-21 15:30:50 -0700

    first test of Xola api and routes

---
+++ app/controllers/api/adventures_controller.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'net/https'
+
+module Api
+  class AdventuresController < ApplicationController
+    respond_to :json
+
+    def show
+      # Return all experiences from Xola api based on form filters and sample one
+      response = Net::HTTP.get_response(URI("https://dev.xola.com/api/experiences?limit=1&category=Photography"))
+      @experiences = response.body
+    end
+  end
+end
