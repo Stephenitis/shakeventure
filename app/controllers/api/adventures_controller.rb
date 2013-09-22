243dfe29902cf651b1754fdf290ea4421c812478
Steven Harlow <STHarlow@gmail.com>
2013-09-21 21:31:34 -0700

    progress toward using filters to pull from xola

--- app/controllers/api/adventures_controller.rb
+++ app/controllers/api/adventures_controller.rb
@@ -6,9 +6,45 @@ module Api
     respond_to :json
 
     def show
-       # Return all experiences from Xola api based on form filters and sample one
-      response = Net::HTTP.get_response(URI("https://dev.xola.com/api/experiences?limit=1&category=Wilderness%20Training"))
+      string = ''
+      string = set_values_from_params(params).stringify_values if params['category']
+      p string
+      # Return all experiences from Xola api based on form filters and sample one
+      response = Net::HTTP.get_response(URI("https://dev.xola.com/api/experiences?category=Wilderness%20Training"))
       @experiences = response.body
+      p params
+      p '*' * 50
     end
+    private
+
+    def set_values_from_params(params)
+      p 'I"M HERE'
+      p params
+      values = {}
+      # values['location'] = params['location'] || 'City, State'
+      # values['price_range'] = params['price_range']
+
+      values['category'] = collect_categories(params['category'])
+    end
+
+    def stringify_values
+      string = '?'
+      self.each_pair do |key, value|
+        string_to_add = "#{key}=#{value}"
+        string << string_to_add
+      end
+      string
+    end
+
+    def collect_categories(category_params)
+      categories = []
+      category_params.each_value do |cat|
+        cat.upcase.constantize.each do |xola_cat|
+          categories << xola_cat
+        end
+      end
+      categories.join(',')
+    end
+
   end
 end
