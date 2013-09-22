383a6669cbe65c2cdd192ff57cf869b2617c2305
Steven Harlow <STHarlow@gmail.com>
2013-09-21 17:27:53 -0700

    create filter form

--- app/controllers/api/adventures_controller.rb
+++ app/controllers/api/adventures_controller.rb
@@ -7,7 +7,7 @@ module Api
 
     def show
       # Return all experiences from Xola api based on form filters and sample one
-      response = Net::HTTP.get_response(URI("https://dev.xola.com/api/experiences?limit=1&category=Photography"))
+      response = Net::HTTP.get_response(URI("https://dev.xola.com/api/experiences?limit=1&category=Wilderness%20Training"))
       @experiences = response.body
     end
   end
